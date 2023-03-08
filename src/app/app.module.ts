ea5a6a1300c7be28340c3a9df9a103c05ed8336d
Yael Soto <xyarock@gmail.com>
2023-03-07 21:16:26 -0600

    terminado lo basico del header inicio body

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeadComponent } from './head/head.component';
+import { BodyComponent } from './body/body.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeadComponent
+    HeadComponent,
+    BodyComponent
   ],
   imports: [
     BrowserModule,
